07f90ff95c4b7822beb6be57ea92039cfeb7bc9e
ConstantineGk <126907082+ConstantineGk@users.noreply.github.com>
2023-06-19 03:24:24 +0300

    Add files via upload

--- NeuralNet_expo.py
+++ NeuralNet_expo.py
@@ -1,17 +1,19 @@
+#----------------------- ConstantineGk ------------------------------------#
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+
 #--------------------------Load training data-----------------------------#
 Train_data = np.loadtxt('Train_MNIST.txt', dtype = 'float', delimiter = ',');
-#---------------------------------------------------------------------------#
-# Predict function
-def predict(X,W1,b,W2,b2):
-    X = np.reshape(X,(1,784))
-    I_H1 = np.dot(X, W1) + b1
-    O_H1 = relu(I_H1)
-    I_OL = np.dot(O_H1, W2) + b2
-    y_hat = sigmoid(I_OL)
-    return y_hat
+
+# Predict function using weights of trainned NN
+def predict(Z,A1,B1,A2,B2):
+    Z = np.reshape(Z,(784,1))
+    W1 = np.dot(A1, Z) + B1
+    Z1 = relu(W1)
+    W2 = np.dot(A2, Z1) + B2
+    y = sigmoid(W2)
+    return y 
 
 # Sigmoid
 def sigmoid(x):
@@ -19,7 +21,7 @@ def sigmoid(x):
 
 # Sigmoid derivative
 def sigmoid_der(x):
-    return sigmoid(x) * (1 - sigmoid(x))
+    return np.exp(-x) / ( (1 + np.exp(-x)) ** 2 )
 
 # Hidden layer activation (ReLu)
 def relu(x):
@@ -28,87 +30,116 @@ def relu(x):
 # ReLU derivative
 def relu_der_(x):
     if x < 0: y = 0
-    elif x > 0: y = 1
+    elif x >= 0: y = 1
     return y
 relu_der = np.vectorize(relu_der_)
 
 # Loss function Cross Entropy
 def cross_Entropy(y_real, y_hat):
-    if y_real == 8:
-        return -np.log(y_hat + 10**-100)
+    y_hat = np.squeeze(y_hat)
+    if y_real == 0:
+        return -np.log(y_hat + (10**-100) )
     else:
-        return -np.log(1 - y_hat + 10**-100)
+        return -np.log(1 - y_hat + (10**-100) )
 
-# Binary Cross-Entropy derivative
+# Cross-Entropy derivative
 def cross_E_grad(y_real, y_hat):
-    if y_real == 8:
-        return - 1/(y_hat + 10**-100)
+    y_hat = np.squeeze(y_hat)
+    if y_real == 0:
+        return - 1/(y_hat + (10**-100) )
     else:
-        return 1/(1 - y_hat + 10**-100)
+        return 1/(1 - y_hat + (10**-100) )
 
 #-------------------------------------------------------------#
 # Initialize the weights and biases for the first and second layers
-W1 = np.random.normal(loc=0, scale=1/(2+20), size=(784, 300))
-W2 = np.random.normal(loc=0, scale=1/(1+20), size=(300, 1))
-b1 = np.zeros((1,300))
-b2 = np.zeros(1).reshape(1,1)
-loss_matrix = [] # Initialize loss matrix
+A1 = np.random.normal(loc=0, scale=1/(300+784), size=(300, 784))
+A2 = np.random.normal(loc=0, scale=1/(1+300), size=(1, 300))
+B1 = np.zeros((300,1))
+B2 = np.zeros(1).reshape(1,1)
+
+# Initialize loss matrix
+loss_matrix = []
+
 # Choose a learning rate
-learning_rate = np.array([0.01]).reshape(1,1)
+learning_rate = 0.01
+
 # Set the number of epochs
-epochs = 80
+epochs = 5
+
+# ADAM parameters
+lamda = 0.1
+c = 0.01
 
 # Train the neural network
 for i in range(epochs):
-    print(i)
-    for j in range(len(Train_data)):
+    np.random.shuffle(Train_data)
+    for iteration in range(len(Train_data)):
+        y_real = Train_data[iteration][-1]
+        Z = Train_data[iteration][:-1]
+        Z = np.reshape(Z,(784,1))
+        
         # Forward pass
-        y_real = Train_data[j][-1]
-        X = Train_data[j][:-1]
-        X = np.reshape(X,(1,784))
-        I_H1 = np.dot(X, W1) + b1
-        O_H1 = relu(I_H1)
-        I_OL = np.dot(O_H1, W2) + b2
-        y_hat = sigmoid(I_OL)
+        W1 = np.dot(A1, Z) + B1
+        Z1 = relu(W1)
+        W2 = np.dot(A2, Z1) + B2
+        y_hat = sigmoid(W2)
+
+        # LOSS
+        loss_matrix.append(cross_Entropy(y_real, y_hat))
 
         # Gradients
-        ce_grad = cross_E_grad(y_real, y_hat)      
-        grad_b2 = ce_grad * sigmoid_der(I_OL)
-        grad_W2 = np.dot(O_H1.T, grad_b2)
-        error_grad_upto_H1 = np.dot(grad_b2 , W2.T)
-        grad_b1 = error_grad_upto_H1 * relu_der(I_H1)
-        grad_W1 = np.dot(X.T, grad_b1)
+        U2 = cross_E_grad(y_real, y_hat)
+        V2 = np.multiply(U2 , sigmoid_der(W2))
+        U1 = np.dot( A2.T, V2)
+        V1 = np.multiply(U1 , relu_der(W1))
 
+        A2_grad = np.dot(V2, Z1.T)
+        B2_grad = V2
+        A1_grad = np.dot(V1,Z.T)
+        B1_grad = V1
+
+        # ADAM
+        if iteration == 0:
+            P_A2 = A2_grad**2
+            P_B2 = B2_grad**2
+            P_A1 = A1_grad**2
+            P_B1 = B1_grad**2
+        else:
+            P_A2 = (1 - lamda) * P_A2 + A2_grad**2
+            P_B2 = (1 - lamda) * P_B2 + B2_grad**2
+            P_A1 = (1 - lamda) * P_A1 + A1_grad**2
+            P_B1 = (1 - lamda) * P_B1 + B1_grad**2
+        
         # Update weights and biases
-        b2 -= learning_rate * grad_b2
-        W2 -= learning_rate * grad_W2
-        b1 -= learning_rate * grad_b1
-        W1 -= learning_rate * grad_W1
+        A2 -= learning_rate * A2_grad / np.sqrt(c + P_A2) 
+        B2 -= learning_rate * B2_grad / np.sqrt(c + P_B2) 
+        A1 -= learning_rate * A1_grad / np.sqrt(c + P_A1) 
+        B1 -= learning_rate * B1_grad / np.sqrt(c + P_B1) 
         
-        # LOSS
-        loss = cross_Entropy(y_real, y_hat)
-        loss_matrix.append(np.squeeze(loss))
-        
-#------------- Normilize Loss for plotting ------------------------#
-chunk_size = 400
+
+# Normilize Loss for plotting
 lista = []
-for i in range(0,epochs):
-    data = sum(loss_matrix[i*chunk_size:i*chunk_size + chunk_size])
+for i in range(0,len(loss_matrix),400):
+    data = sum(loss_matrix[i:i + 400])/400
     lista.append(data)
 plt.plot(lista)
 plt.show()
-#------------------------------------------------------------------#
+#--------------------------#
 print('Trained')
-#----------------------- Load Final Data --------------------------#
+
+# Load Final Data
 Test_data = np.loadtxt('Test_MNIST.txt', dtype = 'float', delimiter = ',');
+
+# Calculate percent error 
 N_mist_8=0
 N_mist_0=0
 for i in range(0,len(Test_data)):
-    y = predict(Test_data[i][:-1],W1,b1,W2,b2)
-    if y<0.5 and Test_data[i][-1] == 0:N_mist_0 += 1
+    y = predict(Test_data[i][:-1],A1,B1,A2,B2)
+    if y<=0.5 and Test_data[i][-1] == 0:N_mist_0 += 1
     if y>0.5 and Test_data[i][-1] == 8:N_mist_8 += 1
-error_p = 0.5 * ( N_mist_0 / len(Test_data)/2) + 0.5 * ( N_mist_8 / len(Test_data)/2) # Calculate percent error 
-print(N_mist_0 / len(Test_data)/2)
-print(N_mist_8 / len(Test_data)/2)
-print(error_p)
+
+error_p = 0.5 * ( N_mist_0 / len(Test_data)) + 0.5 * ( N_mist_8 / len(Test_data))
+
+print( (N_mist_0/len(Test_data)) , (N_mist_8/len(Test_data)) , error_p )
+
 
--- load_mnist.py
+++ load_mnist.py
@@ -1,11 +1,11 @@
 import idx2numpy
 import numpy as np
 #---------------------------------------------------------------------------#
-test_images = 'train-images.idx3-ubyte'
-test_arr = idx2numpy.convert_from_file(test_images).reshape([60000,784]) / 255
+test_images = 't10k-images.idx3-ubyte'
+test_arr = idx2numpy.convert_from_file(test_images).reshape([10000,784]) / 255
 
-test_labels = 'train-labels.idx1-ubyte'
-test_lab = idx2numpy.convert_from_file(test_labels).reshape([60000,1])
+test_labels = 't10k-labels.idx1-ubyte'
+test_lab = idx2numpy.convert_from_file(test_labels).reshape([10000,1])
 
 test_data = np.concatenate((test_arr,test_lab), axis = 1)
 temp = []
@@ -15,11 +15,11 @@ for i in range(0,len(test_data)):
 test_0_8 = np.array(temp)
 #---------------------------------------------------------------------------#
 
-tr_images = 't10k-images.idx3-ubyte'
-tr_arr = idx2numpy.convert_from_file(tr_images).reshape([10000,784]) / 255
+tr_images = 'train-images.idx3-ubyte'
+tr_arr = idx2numpy.convert_from_file(tr_images).reshape([60000,784]) / 255
 
-tr_labels = 't10k-labels.idx1-ubyte'
-tr_lab = idx2numpy.convert_from_file(tr_labels).reshape([10000,1])
+tr_labels = 'train-labels.idx1-ubyte'
+tr_lab = idx2numpy.convert_from_file(tr_labels).reshape([60000,1])
 
 train_data = np.concatenate((tr_arr,tr_lab), axis = 1)
 temp0 = []
